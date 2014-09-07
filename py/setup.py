d86e35f1ddd0a2fbc2df6c51da82e30be482fd27
Jonathan Fine <jfine@pytex.org>
2014-09-07 09:40:15 +0100

    Add setup.py, alib/test.py etc.

---
+++ py/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'alib',
+    packages = ['alib'],
+    version = '0.0.0',
+    description = 'A LIB-raray for testing (and other things).',
+    author = 'Jonathan Fine',
+    author_email = 'jfine@pytex.org',
+    url = 'https://github.com/jonathanfine/py-alib',
+    keywords = ['testing'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+)
