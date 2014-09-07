410b471e43e238283a9d255d701bf82e1a6bb578
Jonathan Fine <jfine@pytex.org>
2014-09-07 12:51:27 +0100

    Add unexpected exception test to evaluator.py.

--- py/test_alib/evaluator.py
+++ py/test_alib/evaluator.py
@@ -2,6 +2,7 @@ from alib.script import Script
 from alib.trytools import ReturnValue, ExceptionInstance
 from alib.evaluator import Evaluator
 
+
 def test_eval(src):
     script = Script(src)
     evaluator = Evaluator()
@@ -11,4 +12,9 @@ def test_eval(src):
     return actual[0]
 
 test_eval('2 + 2 == 5') == (['Eq'], [ReturnValue(4), ReturnValue(5)])
-
+test_eval('2 + 2 == 4') == None
+test_eval('1 + [] < 4') == (
+    ['Lt'],
+    [ExceptionInstance(TypeError("unsupported operand type(s) for +: 'int' and 'list'",)),
+     ReturnValue(4)]
+    )
